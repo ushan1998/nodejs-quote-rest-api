400b9cd4de0b00f093dac22b14490a3541f27ee2
UshanAlwis <95735806+UshanAlwis@users.noreply.github.com>
2022-08-31 16:33:47 +0530

    Mongo DB Connection

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+//Create a Express App
 const app =  express();
 
-//Calling the First Fetch
+//Connect With the Database
+mongoose.connect('mongodb+srv://nodejs-rest-api-quotes:Database@123@nodejs-quote-rest-api.bamibxj.mongodb.net/?retryWrites=true&w=majority',{
+  useNewUrlParser:true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.once('open',() =>{
+    console.log("Connect to the MongoDb");
+} )
+
+
+ // Middleware
+app.use(bodyParser.json());
+
+
+//Routes
 app.get('/',(req, res) =>{
   res.send("Hellow world !!")
 })
 
-// Calling the About
-app.get('/about' ,(req,res) =>{
-  res.send("Hellow, About !!")
-})
 
-// Start the Server
+// Starting the Server
 app.listen(3000, console.log("Listening on the port 3000 !"))
